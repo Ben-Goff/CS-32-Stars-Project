fdc168c58a164c781b644760a587f6de568ba40c
unknown <benpgoff@gmail.com>
2021-02-05 16:04:12 -0600

    implemented extensible kdtree class

---
+++ src/main/java/edu/brown/cs/student/util/Coordinate.java
@@ -0,0 +1,8 @@
+package edu.brown.cs.student.util;
+
+import java.util.ArrayList;
+
+public interface Coordinate {
+
+  public ArrayList<Double> getCoordinate();
+}
---
+++ src/main/java/edu/brown/cs/student/util/KDTree.java
@@ -0,0 +1,2 @@
+package edu.brown.cs.student.util;public class KDTree {
+}
